b638c815cfd1d91abaed41ae98e5de1254bdb4a8
Dominik Kastner <dominikkastner97@yahoo.de>
2019-05-28 13:33:15 +0200

    Initial commit with Main Menu

---
+++ Exploring Namibia/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    
+    //To Call Options from Main Menu
+    public void OptionsMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //To call Main Menu
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
