2fd7085bc139d802c0822555c20bfe9685ccd9a4
Dominik Kastner <dominikkastner97@yahoo.de>
2019-06-05 21:15:42 +0200

    First prototyp of Minigame 1

---
+++ Exploring Namibia/Assets/Scripts/ItemController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    private static int itemsCollected = 0;
+
+    private static int itemCounter = 0;
+
+    private static int spriteCount = 4;
+    private readonly float minSpeed = 4.0f;
+    private readonly float maxSpeed = 8.0f;
+
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] itemSprites = new Sprite[spriteCount];
+
+    private int RndSpriteIndex()
+    {
+        return (int)Random.Range(0, spriteCount);
+    }
+
+    private Vector2 RndStartV()
+    {
+        float y = 5.0f;
+        float x = Random.Range(-5.1f, 5.1f);
+
+        return new Vector2(x, y);
+    }
+
+    private void Start()
+    {
+        this.gameObject.name = "item_" + itemCounter;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = itemSprites[RndSpriteIndex()];
+
+        // Mehr Informationen ueber das Item hier spezifizieren:
+        // Ist das Essen fuer das Tier brauchbar?
+        // Falls ja dann ++score ansonsten --score
+
+        Vector3 startV = RndStartV();
+        this.transform.position = startV;
+    }
+
+    private void SpawnItem()
+    {
+        ++itemCounter;
+        Instantiate(this.gameObject);
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            ++itemsCollected;
+            Debug.Log("Items collected: " + itemsCollected);
+            this.SpawnItem();
+        }
+        
+    }
+
+    void Update()
+    {
+        this.transform.position += new Vector3(0, - Random.Range(minSpeed, maxSpeed)) * Time.deltaTime;
+
+        if(this.transform.position.y < -5.5f)
+        {
+            this.SpawnItem();
+        }
+    }
+}
---
+++ Exploring Namibia/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigid2D;
+    private float x, y;
+    private readonly float speed = 10.0f;
+
+    private void Start()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+        rigid2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+        rigid2D.gravityScale = 0.0f;
+        rigid2D.drag = 10.0f; // removes inertia
+    }
+
+    void Update()
+    {
+        /* Movement with Mouse */
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mouseV = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            this.rigid2D.position = mouseV;
+        }
+
+        /* Movement with Keyboard */
+        else
+        {
+            x = Input.GetAxisRaw("Horizontal");
+            y = Input.GetAxisRaw("Vertical");
+
+            Vector2 velocity = new Vector2(x, y);
+            velocity.Normalize();
+            velocity *= speed;
+            this.rigid2D.velocity = velocity;
+        }
+    }
+}
